ba3862fc2a8d14ba6153f3b85d621af809f29234
Thomas Christensen <Thomas.Christensen@dpi.wi.gov>
2018-02-17 08:41:13 -0600

    Added EF audit console.writeline

--- QuickAudit.ConsoleApp/Program.cs
+++ QuickAudit.ConsoleApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using QuickAudit.Domain;
 
 namespace QuickAudit.ConsoleApp
@@ -15,11 +17,41 @@ namespace QuickAudit.ConsoleApp
                 foreach (var person in persons)
                 {
                     Console.WriteLine($"{person.FirstName} {person.LastName}");
+                    if (person == db.Persons.First())
+                    {
+                        person.LastName += "!";
+                    }
                 }
+
+                db.Persons.Add(new Person
+                {
+                    FirstName = "Jamie",
+                    LastName = "Yup"
+                });
+                db.SaveChanges();
             }
 
             Console.WriteLine("Press a key...");
             Console.ReadKey();
+        }
+    }
+
+    public static class ChangeTrackerExtensions
+    {
+        public static void DisplayTrackedEntities(this ChangeTracker changeTracker)
+        {
+            Console.WriteLine("");
+
+            var entries = changeTracker.Entries();
+            foreach (var entry in entries)
+            {
+                var type = entry.Entity.GetType();
+                Console.WriteLine("Entity Name: {0}", type.Name);
+               
+                Console.WriteLine("Status: {0}", entry.State);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("---------------------------------------");
         }
     }
 }
--- QuickAudit.Domain/QuickAuditDbContext.cs
+++ QuickAudit.Domain/QuickAuditDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
@@ -26,6 +27,59 @@ namespace QuickAudit.Domain
                 .AddJsonFile("appsettings.json")
                 .Build();
             optionsBuilder.UseSqlServer(configuration.GetConnectionString("QuickAuditDatabase"));
+        }
+
+        public override int SaveChanges()
+        {
+
+            var changes = from e in ChangeTracker.Entries()
+                where e.State != EntityState.Unchanged
+                select e;
+
+            foreach (var change in changes)
+            {
+                var type = change.Entity.GetType();
+                if (change.State == EntityState.Added)
+                {
+                    // Log Added
+                }
+                else if (change.State == EntityState.Modified)
+                {
+                    // Log Modified
+                    //var item = change.Cast<IEntity>().Entity;
+                    var originalValues = Entry(change.Entity).OriginalValues;
+                    var currentValues = Entry(change.Entity).CurrentValues;
+
+                    foreach (var property in originalValues.Properties)
+                    {
+                        
+                        var original = originalValues[property.Name];
+                        var current = currentValues[property.Name];
+
+                        if (!Equals(original, current))
+                        {
+                            Console.WriteLine($"{property.Name}: {original} => {current}");
+                            //var originalIdx = originalValues[property];
+                            //Console.WriteLine("{6} = {0}.{4} [{7}][{2}] [{1}] --> [{3}]  Rel:{5}",
+                            //    type,
+                            //    change.OriginalValues.GetValue(originalIdx),
+                            //    change.OriginalValues.GetFieldType(originalIdx),
+                            //    change.CurrentValues.GetValue(originalIdx),
+                            //    change.OriginalValues.EntityType.Name.GetName(originalIdx),
+                            //    change.IsRelationship,
+                            //    change.State,
+                                //string.Join(",", change..EntityKeyValues.Select(v => string.Join(" = ", v.Key, v.Value))));
+                        }
+                    }
+
+                }
+                else if (change.State == EntityState.Deleted)
+                {
+                    // log deleted
+                }
+            }
+            // don't forget to save
+            return 0;//base.SaveChanges();
         }
     }
 }
